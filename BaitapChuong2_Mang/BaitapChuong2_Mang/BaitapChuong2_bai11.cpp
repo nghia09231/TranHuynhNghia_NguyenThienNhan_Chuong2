edaa225b5cd2f14e93ab2feae1ce8776a177fe1e
nghia09231 <nghia09231@gmail.com>
2024-08-12 02:44:41 +0700

    Thiện Nhân : Bài 11_1 Chương 2: Sắp xếp các phần tử trong ma trận sao cho dòng chẵn tăng và dòng lẻ giảm

---
+++ BaitapChuong2_Mang/BaitapChuong2_Mang/BaitapChuong2_bai11.cpp
@@ -0,0 +1,82 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 50
+
+// Hàm nhập ma trận
+void inputMatrix(int matrix[MAX_SIZE][MAX_SIZE], int* n, int* m) {
+	printf("Nhap so dong (n): ");
+	scanf("%d", n);
+	printf("Nhap so cot (m): ");
+	scanf("%d", m);
+
+	printf("Nhap cac phan tu cua ma tran:\n");
+	for (int i = 0; i < *n; i++) {
+		for (int j = 0; j < *m; j++) {
+			printf("Nhap phan tu a[%d][%d]: ", i, j);
+			scanf("%d", &matrix[i][j]);
+		}
+	}
+}
+
+// Hàm xuất ma trận
+void printMatrix(int matrix[MAX_SIZE][MAX_SIZE], int n, int m) {
+	printf("Ma tran:\n");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("%d\t", matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+// Hàm so sánh để sắp xếp tăng dần
+int compareAsc(const void* a, const void* b) {
+	return (*(int*)a - *(int*)b);
+}
+
+// Hàm so sánh để sắp xếp giảm dần
+int compareDesc(const void* a, const void* b) {
+	return (*(int*)b - *(int*)a);
+}
+
+// Hàm sắp xếp các phần tử trong dòng chẵn tăng và dòng lẻ giảm
+void sortRows(int matrix[MAX_SIZE][MAX_SIZE], int n, int m) {
+	for (int i = 0; i < n; i++) {
+		if (i % 2 == 0) { // Dòng chẵn
+			qsort(matrix[i], m, sizeof(int), compareAsc);
+		}
+		else { // Dòng lẻ
+			qsort(matrix[i], m, sizeof(int), compareDesc);
+		}
+	}
+}
+
+
+// Hàm chính
+int main() {
+	int matrix[MAX_SIZE][MAX_SIZE];
+	int n, m;
+
+	// Nhập ma trận
+	inputMatrix(matrix, &n, &m);
+
+	// Xuất ma trận
+	printf("Ma tran ban dau:\n");
+	printMatrix(matrix, n, m);
+
+	// Bài 1: Sắp xếp các phần tử trong ma trận sao cho dòng chẵn tăng và dòng lẻ giảm
+	sortRows(matrix, n, m);
+	printf("Ma tran sau khi sap xep theo dong:\n");
+	printMatrix(matrix, n, m);
+
+	// Nhập lại ma trận
+	inputMatrix(matrix, &n, &m);
+
+	// Xuất ma trận
+	printf("Ma tran ban dau:\n");
+	printMatrix(matrix, n, m);
+
+
+	return 0;
+}
