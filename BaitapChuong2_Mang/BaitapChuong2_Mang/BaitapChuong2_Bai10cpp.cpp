090e072ffbe5114f88ea68f29a5050be55ac5f65
nghia09231 <nghia09231@gmail.com>
2024-08-12 02:16:57 +0700

    Thiện Nhân : Bài 10_1 Chương 2: Liệt kê các cột có tổng nhỏ nhất trong ma trận

---
+++ BaitapChuong2_Mang/BaitapChuong2_Mang/BaitapChuong2_Bai10cpp.cpp
@@ -0,0 +1,103 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MAX_SIZE 50
+
+// Hàm kiểm tra số hoàn thiện
+int isPerfect(int num) {
+	if (num <= 0) return 0;
+	int sum = 1;
+	for (int i = 2; i <= sqrt((double)num); i++) {
+		if (num % i == 0) {
+			sum += i;
+			if (i != num / i) {
+				sum += num / i;
+			}
+		}
+	}
+	return sum == num && num != 1;
+}
+
+// Hàm kiểm tra số nguyên tố
+int isPrime(int num) {
+	if (num <= 1) return 0;
+	if (num == 2) return 1;
+	if (num % 2 == 0) return 0;
+	for (int i = 3; i <= sqrt((double)num); i += 2) {
+		if (num % i == 0) return 0;
+	}
+	return 1;
+}
+
+// Hàm nhập ma trận
+void inputMatrix(int matrix[MAX_SIZE][MAX_SIZE], int* m, int* n) {
+	printf("Nhap so dong (m): ");
+	scanf("%d", m);
+	printf("Nhap so cot (n): ");
+	scanf("%d", n);
+
+	printf("Nhap cac phan tu cua ma tran:\n");
+	for (int i = 0; i < *m; i++) {
+		for (int j = 0; j < *n; j++) {
+			printf("Nhap phan tu a[%d][%d]: ", i, j);
+			scanf("%d", &matrix[i][j]);
+		}
+	}
+}
+
+// Hàm xuất ma trận
+void printMatrix(int matrix[MAX_SIZE][MAX_SIZE], int m, int n) {
+	printf("Ma tran:\n");
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%d\t", matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+// Bài 1: Liệt kê các cột có tổng nhỏ nhất trong ma trận
+void listColumnsWithMinSum(int matrix[MAX_SIZE][MAX_SIZE], int m, int n) {
+	int minSum = 1e9; // Khởi tạo với giá trị lớn
+	int columnSum[MAX_SIZE] = { 0 };
+
+	for (int j = 0; j < n; j++) {
+		int sum = 0;
+		for (int i = 0; i < m; i++) {
+			sum += matrix[i][j];
+		}
+		columnSum[j] = sum;
+		if (sum < minSum) {
+			minSum = sum;
+		}
+	}
+
+	printf("Cac cot co tong nho nhat (%d):\n", minSum);
+	for (int j = 0; j < n; j++) {
+		if (columnSum[j] == minSum) {
+			printf("Cot %d\n", j);
+		}
+	}
+}
+
+
+
+// Hàm chính
+int main() {
+	int matrix[MAX_SIZE][MAX_SIZE];
+	int m, n;
+
+	// Nhập ma trận
+	inputMatrix(matrix, &m, &n);
+
+	// Xuất ma trận
+	printMatrix(matrix, m, n);
+
+	// Bài 1: Liệt kê các cột có tổng nhỏ nhất trong ma trận
+	listColumnsWithMinSum(matrix, m, n);
+
+	
+
+	return 0;
+}
